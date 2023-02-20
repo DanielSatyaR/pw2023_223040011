67d1e32f25696d0694619308e5c124d792615eb6
DanielSatyaR <satyadanielramadhan15@gmail.com>
2023-02-20 17:35:17 +0700

    tugas

---
+++ tugas/1b.php
@@ -0,0 +1,24 @@
+<?php
+$npm = "11";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>B</title>
+</head>
+
+<body>
+    <p>Aku adalah angka <b><?php echo $npm ?></b>
+    </p>
+    <p>jika aku dikali 5 maka aku sekarang menjadi <b><?php echo $npm * 5 ?></b></p>
+    <p>jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $npm * 5 / 2 ?></b></p>
+    <p>jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $npm * 5 / 2 + 75 ?></b></p>
+    <p>jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $npm * 5 / 2 + 75 - 20 ?></b></p>
+</body>
+
+</html>
