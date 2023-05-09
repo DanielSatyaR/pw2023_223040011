3ad8e2f4262cdeb8bacf0afb20e00a070e7aaf43
DanielSatyaR <satyadanielramadhan15@gmail.com>
2023-05-09 10:36:02 +0700

    first commit

---
+++ tubes/halamanUtama.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>How To Learn</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="styleHal.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <script src="https://kit.fontawesome.com/aa15b9abd1.js" crossorigin="anonymous"></script>
+
+
+    <style>
+        .special {
+            font-size: 130px;
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary-subtle">
+        <div class="container">
+
+            <a class="navbar-brand" href="#">How To Learn</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Matematika</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fisika</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kimia</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="header">
+        <p>Pelajaran Sekolah</p>
+    </div>
+    <div class="footer">
+        <div class="icon">
+            <i class="material-symbols-outlined special">
+                calculate
+            </i>
+            <span class="material-symbols-outlined special">
+                pill
+            </span>
+            <span class="material-symbols-outlined special">
+                lab_research
+            </span>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
